7b5e953b0174d26c105e1a530843d05a2ddb9ada
Kevin Python <kevinlee577@yahoo.com>
2019-12-25 00:22:21 -0600

    Modified Publications pages

--- Pages/Publications/Index.cshtml.cs
+++ Pages/Publications/Index.cshtml.cs
@@ -19,11 +19,67 @@ namespace WebAppCapellaKM_05.Pages.Publications
             _context = context;
         }
 
-        public IList<Publication> Publication { get;set; }
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        public PaginatedList<Publication> Publication { get; set; }
+
+    //    public IList<Publication> Publication { get;set; }
+
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
-            Publication = await _context.Publication.ToListAsync();
+            CurrentSort = sortOrder;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            CurrentFilter = searchString;
+
+            IQueryable<Publication> studentsIQ = from s in _context.Publication
+                                             select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                studentsIQ = studentsIQ.Where(s => s.PublicationName.Contains(searchString));
+
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.PublicationName);
+                    break;
+                case "Date":
+                    studentsIQ = studentsIQ.OrderBy(s => s.PublicationName);
+                    break;
+                case "date_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.PublicationName);
+                    break;
+                default:
+                    studentsIQ = studentsIQ.OrderBy(s => s.PublicationName);
+                    break;
+            }
+
+                int pageSize = 5;
+
+                Publication = await PaginatedList<Publication>.CreateAsync(
+                studentsIQ.AsNoTracking(),
+                pageIndex ?? 1,
+                pageSize);
+
+            //   Publication = await _context.Publication.ToListAsync();
+
+
         }
     }
 }
