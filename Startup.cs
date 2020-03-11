6e512895d330f4b8efffd7d779a053ab7dc1ccf1
Kevin Python <kevinlee577@yahoo.com>
2020-03-10 21:24:30 -0500

    Update Startup.cs file.

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,8 @@ using WebAppCapellaKM_05.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+
 namespace WebAppCapellaKM_05
 {
     public class Startup
@@ -24,6 +26,7 @@ namespace WebAppCapellaKM_05
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
