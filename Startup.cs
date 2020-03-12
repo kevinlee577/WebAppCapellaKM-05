b3e01bea7e630bad658f9518ba300baf2a10d6af
Kevin Python <kevinlee577@yahoo.com>
2020-03-11 21:55:11 -0500

    Removed HTTP directive in Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -12,7 +12,6 @@ using WebAppCapellaKM_05.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Http;
 
 namespace WebAppCapellaKM_05
 {
